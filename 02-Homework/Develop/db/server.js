33a0a2b8344948ee56bbd651d6c5d1ee8b717787
Glimpz <abgabbard@gmail.com>
2021-02-04 18:06:02 -0700

    successfully appending db.json file added html routes and api routes

--- 02-Homework/Develop/db/server.js
+++ 02-Homework/Develop/db/server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
-var notes = [];
+var path = require("path");
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
