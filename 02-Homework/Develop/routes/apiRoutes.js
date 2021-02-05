f0b74773a5f358cdda126245408d1e34feb60fab
Glimpz <abgabbard@gmail.com>
2021-02-05 13:57:34 -0700

    created generateUniqueID function for storing notes in JSON. This will allow the user to later delete the message using this unique ID

--- 02-Homework/Develop/routes/apiRoutes.js
+++ 02-Homework/Develop/routes/apiRoutes.js
@@ -1,6 +1,12 @@
 var fs = require("fs");
 var path = require("path");
 var notes = require("../db/db.json");
+const bodyParser = require("body-parser")
+const crypto = require("crypto");
+
+function generateUniqueID() {
+  return crypto.randomBytes(8).toString('hex') 
+} 
 
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
@@ -10,6 +16,9 @@ module.exports = function (app) {
     const file = path.join(__dirname,"../db/db.json");
     const newNote = req.body;
 
+    newNote.id = generateUniqueID();
+
+
     notes.push(newNote);
 
     fs.appendFile(file,JSON.stringify(newNote, null, 4) ,err => {
@@ -18,4 +27,19 @@ module.exports = function (app) {
     });
     res.send(newNote);
 });
+  // app.delete("/api/notes/:id")
+  // const id = req.param.id;
+  // const file = path.join(__dirname, "../db/db.json");
+
+  // for(const note of notes) {
+  //   if (id === note.id) {
+  //     const index = notes.indexOf(note);
+  //     notes.splice(index, 1);
+  //     fs.writeFile(file, JSON.stringify(notes, null, 4), err => {
+  //       if (err) throw err;
+  //       console.log("Your note was deleted.");
+  //     })
+  //     res.end();
+  //   }
+  // }
 };
