acd0dbb849a7dcb45b203c35aa969f57c056b152
Glimpz <abgabbard@gmail.com>
2021-02-06 09:53:57 -0700

    updated apiRoutes with correct param(s).id. Previously the delete button was deleting more than just the unique ID it was supposed to

--- 02-Homework/Develop/routes/apiRoutes.js
+++ 02-Homework/Develop/routes/apiRoutes.js
@@ -18,21 +18,20 @@ module.exports = function (app) {
 
     newNote.id = generateUniqueID();
 
-
     notes.push(newNote);
 
-    fs.appendFile(file,JSON.stringify(newNote, null, 4) ,err => {
+    fs.writeFile(file,JSON.stringify(notes, null, 4) ,err => {
         if (err) throw err;
         console.log("note saved!");
     });
     res.send(newNote);
 });
   app.delete("/api/notes/:id", (req, res) => {
-  const id = req.param.id;
+  const id = req.params.id;
   const file = path.join(__dirname, "../db/db.json");
 
   for(const note of notes) {
-    if (id === notes.id) {
+    if (id === note.id) {
       const index = notes.indexOf(note);
       notes.splice(index, 1);
       fs.writeFile(file, JSON.stringify(notes, null, 4), err => {
